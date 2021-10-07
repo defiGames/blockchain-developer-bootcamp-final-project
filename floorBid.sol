6b6db1850840cc8a62b24f2cdd72fc0478faaf29
defiGames <88115149+defiGames@users.noreply.github.com>
2021-10-07 10:43:04 -0700

    outlining functions

---
+++ floorBid.sol
@@ -0,0 +1,18 @@
+function approve(address project){
+    //approve a bid for a project
+}
+function addBid(address project){
+    //adds a min bid for a project with the project contract id and value sent
+}
+
+function removeBid(address project){
+    // removes current bid on a given project and refunds user
+}
+
+function purchase(){
+    //run by keeper, purchase NFTS that are under floor Bid
+}
+
+function getHighestBid(address project){
+    //view highest bid on a project
+}
