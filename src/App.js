a6287548cdf32004c52234431a7c7bae311f378c
defiGames <88115149+defiGames@users.noreply.github.com>
2021-11-12 16:45:12 -0800

    cleaned up front end

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import Rarity from './artifacts/contracts/Rarity.sol/Rarity.json'
 
 // Update with the contract address logged out to the CLI when it was deployed 
 const rarityAddress = "0x5f3f1dBD7B74C6B46e8c44f98792A1dAf8d69154"
-  let provider 
-  let signer
-  let contract 
-  let accounts
-  let address
+let provider 
+let signer
+let contract 
+let accounts
+let address
+
 function App() {
 
   const [pattern, setPatternValue] = useState()
@@ -17,73 +18,97 @@ function App() {
   const [msg, setMsg] = useState()
   const [walletStatus, setWalletStatus] = useState("Connect Wallet")
   const [reward, setReward] = useState()
-  //load account info on page load of request wallet connection
+
   useEffect(() => {
-    isMetaMaskConnected() 
-//function here to initialize, other function should 
-    checkReward()
-    fetchPatterns()
+    initialize()
   },[])
   
-  // request access to the user's MetaMask account
-
-  async function isMetaMaskConnected() {
+  async function connectWallet() {
+    if(walletConnected())return
+      accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      initialize()
+  }
+  
+  async function initialize() {
     if (window.ethereum) {
-      //maybe try accounts here and return if good
       try {
+        //console.log("initializing")
+        accounts = await window.ethereum.request({ method: 'eth_accounts' });
         provider = new ethers.providers.Web3Provider(window.ethereum)
         signer = provider.getSigner()
         address = await signer.getAddress()
         contract = new ethers.Contract(rarityAddress, Rarity.abi, signer)
-        accounts = await window.ethereum.request({ method: 'eth_accounts' });
-        const filter = contract.filters.newPattern(address, 5)
-        contract.on(filter, (  metadata, event) => {
-        console.log('Address  :', metadata);
-        console.log('Pattern Id : ', event.toString());  //Event object
-    });
+
+        const eventFilter = contract.filters.newPattern(address, null)
+        
+        //set event listener
+        contract.on(eventFilter, (  metadata, event) => {
+          console.log('Address  :', metadata);
+          console.log('Pattern Id : ', event.toString());  //Event object
+          setMsg(`Pattern ID No. ${event.toString()} has been submitted!`)
+          refreshAccount()
+        });
+        
       } catch (err) {
-        setMsg("Error connecting Wallet: "+ err.message)
+        console.log("Error connecting Wallet: "+ err.message)
+        setMsg("Please Connect Wallet")
         return false
       }
-      if (accounts.length > 0) {
-        setWalletStatus("Wallet Connected")
-      } else {
-        setWalletStatus("Connect Wallet")
-      }
+      refreshAccount()
     } else {
       setMsg("Please install Metamask")
       return false
     }
   }
 
-  async function checkReward() {
-    if (!isMetaMaskConnected()) return
-    try {
-      const _reward = ethers.utils.formatEther(await contract.checkReward())
-      setReward(`Reward: ${_reward} Eth`)
-    } catch (err) {
-      console.log("Error: ", err)
+  function refreshAccount() { 
+      fetchPatterns()
+      checkReward()
+  }
+
+  function walletConnected() {
+    if (window.ethereum && accounts && accounts.length > 0) {
+      setWalletStatus("Wallet Connected")
+      return true
+    } else {
+      setWalletStatus("Connect Wallet")
+      setMsg("Please Connect Wallet")
+      return false
     }
-    setMsg("")
   }
 
-  // call the smart contract, read the current pattern id
-  async function confirmation() {
-
-    if (!isMetaMaskConnected()) return
-      try {
-        const data = String(await contract.totalPatternCount())
-        setMsg(`Pattern ID No. ${data-1} has been submitted!`)
-      } catch (err) {
-        console.log("Error: ", err)
-      }
-        
+  async function submitPattern() {
+    if (!walletConnected()) return
+    if (!pattern || pattern.length !== 4) {
+      setMsg("please enter the correct amount of digits")
+      return
+    }
+    let overrides = {
+      value: ethers.utils.parseEther("0.1")     // ether in this case MUST be a string
+    }; 
+    console.log("sending tx");
+    let transaction;
+    try {
+      transaction = await contract.mintGuess([parseInt(pattern[0]),parseInt(pattern[1]),parseInt(pattern[2]),parseInt(pattern[3]),], overrides)
+      console.log("send tx");
+    } catch (err) {
+      setMsg(err.message)
+      return false
+    }
+    setMsg("Tx Pending")
+    /* Instead of using a receipt we are listening for the event now
+    const receipt = await transaction.wait()
+    if(receipt.status === 1) {
+      setMsg("Pattern submission sucessful!")
+    } else {
+      setMsg("There was an error submitting your pattern, please try again")
+      return false;
+    }
+    */
   }
-
+  
   async function fetchPatterns() {
-
-    if (!isMetaMaskConnected()) return
-
+    if (!walletConnected()) return
       try {
         const data = await contract.fetchPatternID()
         let patternText = ""
@@ -102,46 +127,16 @@ function App() {
         }
 
       } catch (err) {
-        console.log("Error: ", err)
+        setMsg(err)
       }
   }
 
-  async function submitPattern() {
-    if (!pattern || pattern.length !== 4) {
-      setMsg("please enter the correct amount of digits")
+  async function claimReward() {
+    if (!walletConnected()) return
+    if (reward == 0) {
+      setMsg("No Reward to Claim")
       return
     }
-    let overrides = {
-      value: ethers.utils.parseEther("0.1")     // ether in this case MUST be a string
-    }; 
-    if (!isMetaMaskConnected()) return
-    console.log("sending tx");
-    let transaction;
-    try {
-      transaction = await contract.mintGuess([parseInt(pattern[0]),parseInt(pattern[1]),parseInt(pattern[2]),parseInt(pattern[3]),], overrides)
-      console.log("send tx");
-    } catch (err) {
-      setMsg(err.message)
-      return false
-    }
-    setMsg("Tx Pending")
-    const receipt = await transaction.wait()
-    if(receipt.status === 1) {
-      setMsg("Pattern submission sucessful!")
-    } else {
-      setMsg("There was an error submitting your pattern, please try again")
-      return false;
-    }
-    //const gasUsed = receipt.status;
-    console.log(receipt.status);
-    fetchPatterns()
-    checkReward()
-    confirmation()
-  
-  }
-  
-  async function claimReward() {
-    if (!isMetaMaskConnected()) return
     let claim
     try {
     claim = await contract.claimReward();
@@ -151,15 +146,26 @@ function App() {
     }
     setMsg("Tx Pending")
     const receipt = await claim.wait()
-    await checkReward() 
-
-    if(receipt.status === 1) {
-      setMsg("Reward Claimed Sucessfully")
+    //wait for transaction to finish
+    //check the reward amount, update msg, check that reward is zeroed out
+    const _reward = await checkReward() 
+    if(receipt.status === 1 && _reward == 0) {
+      setMsg(reward + " Eth Reward Claimed Sucessfully")
     } else {
       setMsg("There was an error, please try again")
       return false;
     }
-  
+  }
+
+  async function checkReward() {
+    try {
+      const _reward = ethers.utils.formatEther(await contract.checkReward())
+      const res = (+_reward).toFixed(2);
+      setReward(res)
+      return _reward
+    } catch (err) {
+      setMsg(err)
+    }
   }
 
   return (
@@ -169,11 +175,11 @@ function App() {
         <input onChange={e => setPatternValue(e.target.value)} placeholder="Submit Pattern" />
         <button onClick={submitPattern}>Save Pattern</button>
         <div className="App-confirmation">{msg}</div>
-        <button onClick={fetchPatterns}>View My Patterns</button>
+        <div className="app-pattern-title">Your Patterns</div>
         <div className="app-pattern">{savedPatterns}</div>
         <button onClick={claimReward}>Claim Reward</button>
-        <div className="app-claim">{reward}</div>
-        <button onClick={isMetaMaskConnected}>{walletStatus}</button>
+        <div className="app-claim">Reward: {reward} Eth</div>
+        <button onClick={connectWallet}>{walletStatus}</button>
      </header>
     </div>
   );
