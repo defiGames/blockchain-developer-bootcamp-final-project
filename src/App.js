eabc630b5423552582bc3300748d48f27f9921d3
defiGames <88115149+defiGames@users.noreply.github.com>
2021-11-23 19:08:09 -0800

    working on stlye

--- src/App.js
+++ src/App.js
@@ -9,19 +9,21 @@ let provider
 let signer
 let contract 
 let accounts
-let address
 const  numSquares = 9
+let  network  
 
 function App() {
 
   const [savedPatterns, setSavedPatterns] = useState()
   const [msg, setMsg] = useState()
   const [walletStatus, setWalletStatus] = useState("Connect Wallet")
-  const [reward, setReward] = useState()
+  const [reward, setReward] = useState("0.0")
   const sp = new Array(numSquares).fill(0)
   const [squarePattern, setSquarePattern] = useState(sp)
   const s = new Array(numSquares)
   const [style, setStyle] = useState(s)
+  const [patternLimit, setPatternLimit] = useState(0)
+  const [address, setAddress] = useState()
 
   useEffect(() => {
     initialize()
@@ -39,9 +41,12 @@ function App() {
         //console.log("initializing")
         accounts = await window.ethereum.request({ method: 'eth_accounts' });
         provider = new ethers.providers.Web3Provider(window.ethereum)
+        network  = await provider.getNetwork()
         signer = provider.getSigner()
-        address = await signer.getAddress()
+        setAddress( await signer.getAddress())
         contract = new ethers.Contract(rarityAddress, Rarity.abi, signer)
+        setPatternLimit( await contract.patternLimit())
+        console.log(patternLimit.toString())
 
         const eventFilter = contract.filters.newPattern(address, null)
         
@@ -72,10 +77,16 @@ function App() {
 
   function walletConnected() {
     if (window.ethereum && accounts && accounts.length > 0) {
+      console.log(network.chainId)  
+      if (network.chainId !== 1337){ //change this when deploying
+        setMsg("Please connect to correct Network")
+        return false
+      }
       setWalletStatus("Wallet Connected")
       return true
     } else {
       setWalletStatus("Connect Wallet")
+      setAddress("")
       setMsg("Please Connect Wallet")
       return false
     }
@@ -132,7 +143,7 @@ function App() {
 
   async function claimReward() {
     if (!walletConnected()) return
-    if (reward == 0) {
+    if (reward === 0) {
       setMsg("No Reward to Claim")
       return
     }
@@ -148,7 +159,7 @@ function App() {
     //wait for transaction to finish
     //check the reward amount, update msg, check that reward is zeroed out
     const _reward = await checkReward() 
-    if(receipt.status === 1 && _reward == 0) {
+    if(receipt.status === 1 && _reward === 0) {
       setMsg(reward + " Eth Reward Claimed Sucessfully")
     } else {
       setMsg("There was an error, please try again")
@@ -186,40 +197,55 @@ function App() {
         //<input onChange={e => setPatternValue(e.target.value)} placeholder="Submit Pattern" />
   return (
     <div className="App">
-      <header className="App-header">
-        <div className="Instructions">Please Enter a 4 digit Pattern of 1s and 0s</div>
-
-        <div className="game">
-        <div className="game-board">
-        <div>
-          <div className="board-row">
-            <button className="square" style={style[0]} onClick={() =>squareClicked(0)}></button>
-            <button className="square" style={style[1]} onClick={() => squareClicked(1)}></button>
-            <button className="square" style={style[2]} onClick={() =>squareClicked(2)}></button>
-          </div>
-          <div className="board-row">
-            <button className="square" style={style[3]} onClick={() =>squareClicked(3)}></button>
-            <button className="square" style={style[4]} onClick={() =>squareClicked(4)}></button>
-            <button className="square" style={style[5]} onClick={() =>squareClicked(5)}></button>
-          </div>
-          <div className="board-row">
-            <button className="square" style={style[6]} onClick={() =>squareClicked(6)}></button>
-            <button className="square" style={style[7]} onClick={() =>squareClicked(7)}></button>
-            <button className="square" style={style[8]} onClick={() =>squareClicked(8)}></button>
-          </div>
+      <div className="topBar">
+        <div className="claim">
+          <button onClick={claimReward}>Claim Reward</button>
+          <span className="reward">Reward: {reward} Eth</span>
         </div>
 
+        <div className="wallet">
+          <button onClick={connectWallet}>{walletStatus}</button>
+          <div className="address">{address && "..." + address.slice(-5)}</div>
         </div>
-      </div> 
+      </div>
+      <h1 className="Title">The Rarity Game</h1>
+      <div className="gameContainer">
+        <div className="gameAndButton">
+          <div className="game">
+            <div className="game-board">
+              <div className="board-row">
+                <button className="square" style={style[0]} onClick={() =>squareClicked(0)}></button>
+                <button className="square" style={style[1]} onClick={() => squareClicked(1)}></button>
+                <button className="square" style={style[2]} onClick={() =>squareClicked(2)}></button>
+              </div>
+              <div className="board-row">
+                <button className="square" style={style[3]} onClick={() =>squareClicked(3)}></button>
+                <button className="square" style={style[4]} onClick={() =>squareClicked(4)}></button>
+                <button className="square" style={style[5]} onClick={() =>squareClicked(5)}></button>
+              </div>
+              <div className="board-row">
+                <button className="square" style={style[6]} onClick={() =>squareClicked(6)}></button>
+                <button className="square" style={style[7]} onClick={() =>squareClicked(7)}></button>
+                <button className="square" style={style[8]} onClick={() =>squareClicked(8)}></button>
+              </div>
+            </div>
+          </div>
 
-        <button onClick={submitPattern}>Save Pattern</button>
-        <div className="App-confirmation">{msg}</div>
-        <div className="app-pattern-title">Your Patterns</div>
+          <button onClick={submitPattern}>Save Pattern</button>
+          <div className="App-confirmation">{msg}</div>
+        </div>
+        <div className="Instructions">Rules: Please create and submit a unique pattern. After the pattern limit of {patternLimit.toString()} patterns is reached,
+         each new pattern submission will trigger a burn of the most common pattern submission.
+        A submissionn fee of 0.1 Eth is divided up amongst existing active patterns creators.
+        The longer your pattern stays active, the more rewards you earn!.
+        You can withdraw rewards earned at anytime.
+        </div>
+
+      </div>
+
+        <h2 className="app-pattern-title">Your Patterns</h2>
         <div className="app-pattern">{savedPatterns}</div>
-        <button onClick={claimReward}>Claim Reward</button>
-        <div className="app-claim">Reward: {reward} Eth</div>
-        <button onClick={connectWallet}>{walletStatus}</button>
-     </header>
+
     </div>
   );
 }
